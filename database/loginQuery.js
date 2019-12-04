817b4fc4b7df783775dc7c8807fcac16ece9a495
justalak <49803526+justalak@users.noreply.github.com>
2019-12-04 15:02:06 +0700

    remove bcrypt

--- database/loginQuery.js
+++ database/loginQuery.js
@@ -1,6 +1,7 @@
 
-var bcrypt = require('bcrypt');
+
 var db =require('./createPool');
+var hashPassword=require('password-hash');
 
 module.exports = {
     getUser: async (user) => {
@@ -8,8 +9,9 @@ module.exports = {
         try {
             result = await db.query('select * from user where username=? ', [user['username']]);
             var dbpassword = result[0][0].password;
-            var res = await bcrypt.compareSync(user['password'], dbpassword);
-
+            // var res = await bcrypt.compareSync(user['password'], dbpassword);
+            var res=hashPassword.verify(user.password,dbpassword);
+            console.log(res);
             if (!res) return false;
             else return result[0][0];
         } catch{
@@ -19,8 +21,9 @@ module.exports = {
     addUser: async (user) => {
         var result;
         try {
-            var salt = await bcrypt.genSaltSync(10);
-            var password = await bcrypt.hashSync(user['password'], salt);
+            // var password = await bcrypt.hashSync(user['password'], salt);
+            var password= hashPassword.generate(user.password);
+            console.log(password);
             await db.query('insert into user(username,password,firstname,lastname,email,gender) values(?,?,?,?,?,?)',
                 [user['username'], password, user['firstname'], user['lastname'], user['email'], user['gender']]
             );
